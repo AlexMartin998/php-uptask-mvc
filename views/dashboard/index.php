88f0bbc499a89c81f718e83c6b342db75dd663b9
Alex Martin <alextravc.342@gmail.com>
2023-03-24 21:59:49 -0500

    [ADD]: show projects that belongs to auth user

--- views/dashboard/index.php
+++ views/dashboard/index.php
@@ -1,5 +1,18 @@
 <?php include_once __DIR__ . '/../dashboard/header-dashboard.php' ?>
 
 
+<?php if (empty($projects)) : ?>
+  <p class="no-projects">No Hay Proyectos Aún <a href="/crear-proyecto">Comienza creando uno</a></p>
+
+<?php else : ?>
+  <ul class="project-list">
+    <?php foreach ($projects as $project) : ?>
+      <li class="project">
+        <a href="/proyecto?id=<?= $project->url ?>"><?= $project->title ?></a>
+      </li>
+    <?php endforeach; ?>
+  </ul>
+<?php endif; ?>
+
 
 <?php include_once __DIR__ . '/../dashboard/footer-dashboard.php' ?>
