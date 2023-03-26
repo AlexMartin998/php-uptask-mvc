5ded73f550a6e93fcb31953ae99408d34e4f2753
Alex Martin <alextravc.342@gmail.com>
2023-03-25 21:23:43 -0500

    [ADD]: navbar mobile design

--- views/templates/navbar.php
+++ views/templates/navbar.php
@@ -1,3 +1,12 @@
+<div class="navbar-mobile">
+  <h1>UpTask</h1>
+
+  <div class="menu">
+    <img id="mobile-menu" src="build/img/menu.svg" alt="imagen menu">
+  </div>
+
+</div>
+
 <div class="navbar">
   <p>Hola <span><?php echo $_SESSION['name']; ?></span></p>
 
